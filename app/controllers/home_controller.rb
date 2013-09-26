5c936aba59f4bc226192c7df978ac1dfd55c1a4f
Pieter Gunst <pietergunst@gmail.com>
2013-09-25 22:54:36 -0700

    web page updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
   end
+  def cs106a
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Codeislaw::Application.routes.draw do
   root :to => "home#index"
+  get "home/cs106a"
 end
